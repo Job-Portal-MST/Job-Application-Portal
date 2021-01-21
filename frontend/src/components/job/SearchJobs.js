ae3222b6680b3fb502ccb5bc0b3bcae9212f6b86
Vedansh Mittal <56213441+VedanshM@users.noreply.github.com>
2021-01-22 01:16:45 +0530

    search filter done

--- frontend/src/components/job/SearchJobs.js
+++ frontend/src/components/job/SearchJobs.js
@@ -6,8 +6,44 @@ import "bootstrap/dist/css/bootstrap.min.css";
 class SearchJobs extends Component {
     constructor() {
         super();
-        this.state = { key: "", jobs: [] };
+        this.state = {
+            key: "",
+            orgJobs: [],
+            showJobs: [],
+            filters: {
+                type: "Any",
+                salaryMin: -Infinity,
+                salaryMax: +Infinity,
+                duration: 7,
+                sortby: "",
+                reverse: false,
+            },
+        };
     }
+
+    applyFilters = (filters) => {
+        if (filters.salaryMax === "") {
+            filters.salaryMax = Infinity;
+        }
+        this.setState({ filters });
+        let jobList = this.state.orgJobs;
+        if (filters.type !== "Any") {
+            jobList = jobList.filter((item) => item.type === filters.type);
+        }
+        if (filters.salaryMin > -Infinity) {
+            jobList = jobList.filter((item) => item.salary >= filters.salaryMin);
+        }
+        if (filters.salaryMax < +Infinity) {
+            jobList = jobList.filter((item) => item.salary <= filters.salaryMax);
+        }
+        jobList = jobList.filter((item) => item.duration < filters.duration);
+        if (filters.sortBy !== "") {
+            jobList.sort(
+                (a, b) => (filters.reverse ? -1 : 1) * (a[filters.sortBy] - b[filters.sortBy])
+            );
+        }
+        this.setState({ showJobs: jobList });
+    };
 
     createApplyButton = (job, index) => {
         if (job.applied === "yes") {
@@ -21,8 +57,8 @@ class SearchJobs extends Component {
 
     sortJobsfunc = (key, reverse = false) => (e) => {
         e.preventDefault();
-        const m = reverse ? -1 : 1;
-        this.setState({ jobs: this.state.jobs.sort((a, b) => m * (a[key] - b[key])) });
+        const filters = { ...this.state.filters, sortBy: key, reverse };
+        this.applyFilters(filters);
     };
 
     configureSection = () => {
@@ -31,35 +67,81 @@ class SearchJobs extends Component {
                 {msg}
             </button>
         );
+        const onChange = (e) => {
+            e.preventDefault();
+            const filters = { ...this.state.filters, [e.target.id]: e.target.value };
+            this.applyFilters(filters);
+        };
         return (
             <div style={{ margin: "10px" }}>
                 <div>
                     {sortBtn("Sort by Salary", this.sortJobsfunc("salary"))}
-                    {sortBtn("Sort by Salary", this.sortJobsfunc("salary", true))}
+                    {sortBtn("Sort by Salary(rev)", this.sortJobsfunc("salary", true))}
                     {sortBtn("Sort by Rating", this.sortJobsfunc("rating"))}
-                    {sortBtn("Sort by Rating", this.sortJobsfunc("rating", true))}
+                    {sortBtn("Sort by Rating(rev)", this.sortJobsfunc("rating", true))}
                     {sortBtn("Sort by Duration", this.sortJobsfunc("duration"))}
-                    {sortBtn("Sort by Duration", this.sortJobsfunc("duration", true))}
+                    {sortBtn("Sort by Duration(rev)", this.sortJobsfunc("duration", true))}
                     <br />
-                    Filter:
-                    <form>
-                        <div className="dropdown">
-                            <select id="type" onChange={console.log}>
-                                <option className="dropdown-item" value="Any">
-                                    Any
-                                </option>
-                                <option className="dropdown-item" value="Work From Home">
-                                    Work From Home
-                                </option>
-                                <option className="dropdown-item" value="Part Time">
-                                    Part Time
-                                </option>
-                                <option className="dropdown-item" value="Full Time">
-                                    Full Time
-                                </option>
-                            </select>
-                        </div>
-                    </form>
+                    <div className="dropdown">
+                        Type:
+                        <select id="type" onChange={onChange}>
+                            <option className="dropdown-item" value="Any">
+                                Any
+                            </option>
+                            <option className="dropdown-item" value="Work From Home">
+                                Work From Home
+                            </option>
+                            <option className="dropdown-item" value="Part Time">
+                                Part Time
+                            </option>
+                            <option className="dropdown-item" value="Full Time">
+                                Full Time
+                            </option>
+                        </select>
+                    </div>
+                    <label>Salary min:</label>
+                    <input id="salaryMin" type="number" min="0" step="1" onChange={onChange} />
+                    <label>Salary max:</label>
+                    <input id="salaryMax" type="number" min="0" step="1" onChange={onChange} />
+                    {/* <input
+                        id="duration"
+                        type="number"
+                        min="1"
+                        max="7"
+                        step="1"
+                        onChange={onChange}
+                    /> */}
+                    <div className="dropdown">
+                        <label>Duration less than:</label>
+                        <select
+                            itemType="number"
+                            defaultValue="7"
+                            id="duration"
+                            onChange={onChange}
+                        >
+                            <option className="dropdown-item" value="1">
+                                1
+                            </option>
+                            <option className="dropdown-item" value="2">
+                                2
+                            </option>
+                            <option className="dropdown-item" value="3">
+                                3
+                            </option>
+                            <option className="dropdown-item" value="4">
+                                4
+                            </option>
+                            <option className="dropdown-item" value="5">
+                                5
+                            </option>
+                            <option className="dropdown-item" value="6">
+                                6
+                            </option>
+                            <option className="dropdown-item" value="7">
+                                7
+                            </option>
+                        </select>
+                    </div>
                 </div>
             </div>
         );
@@ -85,7 +167,8 @@ class SearchJobs extends Component {
                             if (tmp.length > 0) job.applied = "yes";
                             else job.applied = "no";
                         }
-                        this.setState({ jobs: jobList });
+                        this.setState({ orgJobs: jobList });
+                        this.setState({ showJobs: jobList });
                     })
                     .catch((err) => {
                         console.log(err);
@@ -100,7 +183,7 @@ class SearchJobs extends Component {
     applyJob = (index) => (e) => {
         e.preventDefault();
         let url = new URLSearchParams({
-            jobid: this.state.jobs[index]._id,
+            jobid: this.state.showJobs[index]._id,
         });
         url = "/jobapply/?" + url.toString();
         window.location = url;
@@ -109,7 +192,7 @@ class SearchJobs extends Component {
     render() {
         return (
             <div className="container">
-                <h1>Search jobs</h1>
+                <h1>Search Jobs</h1>
                 <form
                     onSubmit={(e) => {
                         e.preventDefault();
@@ -154,7 +237,7 @@ class SearchJobs extends Component {
                             </tr>
                         </thead>
                         <tbody>
-                            {this.state.jobs.map((item, index) => {
+                            {this.state.showJobs.map((item, index) => {
                                 return (
                                     <tr key={index}>
                                         <td onClick={this.applyJob(index)}>{item.title}</td>
